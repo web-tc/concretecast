cddd997a503aff0106efb5b6083327ca43828ef2
imranm <imran@localhost.loc>
2022-04-30 12:14:14 +0100

    Fixed menu items showing if window height is less than 560px.

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 const menuBtn = document.querySelector(".menu-btn");
 const menuItems = document.querySelector(".menu-items");
-const expandBtn = document.querySelectorAll(".expand-btn");
+const expandBtns = document.querySelectorAll(".expand-btn");
 
 // humburger toggle
 menuBtn.addEventListener("click", () => {
+  if (menuBtn.classList.contains('open')) {
+    expandBtns.forEach((btn) => {
+      btn.classList.remove('open');
+    });
+  };
   menuBtn.classList.toggle("open");
   menuItems.classList.toggle("open");
 });
 
 // mobile menu expand
-expandBtn.forEach((btn) => {
+expandBtns.forEach((btn) => {
   btn.addEventListener("click", () => {
     btn.classList.toggle("open");
   });
